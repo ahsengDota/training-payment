96329ab37549caa794cfe4bb20f1aef48560cde8
glenningkan <ahsengdota1989@gmail.com>
2019-09-06 14:37:08 +0800

    initial commit

---
+++ WebAPI2/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using WebAPI2.Models;
+
+namespace WebAPI2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //this service will make this app as web app , if doesn't have this line , then this will become an app instead of web app
+            services.AddMvc()
+                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                 .AddJsonOptions(options => {
+                     var resolver = options.SerializerSettings.ContractResolver;
+                     if (resolver != null)
+                         (resolver as DefaultContractResolver).NamingStrategy = null;
+                 });
+
+            services.AddDbContext<PaymentDetailContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+            //this is to enable CORS , to be used by client side
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            app.UseMvc();
+        }
+    }
+}
